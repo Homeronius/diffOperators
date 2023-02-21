9c2f1988f6b6699ceacb3f31ce04370aa6c72afa
Tobia Clagluena <homeronius8050@gmail.com>
2023-02-21 11:12:53 +0100

    remove obsolete free  function

--- field.h
+++ field.h
@@ -21,18 +21,6 @@ typedef Eigen::Vector3d vector3d_t;
 typedef Eigen::Vector3i vector3i_t;
 typedef Eigen::Matrix3d matrix3d_t;
 
-// Taken from https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
-template<class T>
-typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
-    almost_equal(T x, T y, int ulp)
-{
-    // the machine epsilon has to be scaled to the magnitude of the values used
-    // and multiplied by the desired precision in ULPs (units in the last place)
-    return std::fabs(x-y) <= std::numeric_limits<T>::epsilon() * std::fabs(x+y) * ulp
-        // unless the result is subnormal
-        || std::fabs(x-y) < std::numeric_limits<T>::min();
-}
-
 // Index struct to pass raveled / unraveled indices to operators / fields
 struct Index {
     typedef std::tuple<size_t,size_t,size_t> idx_tuple_t;
